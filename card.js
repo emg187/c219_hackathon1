2c94ba62845150d6d60ad80d662d6399071d909c
Quan Le <quandhle@gmail.com>
2019-03-06 09:39:47 -0800

    created all files for basic classes

---
+++ card.js
@@ -0,0 +1,15 @@
+class Card {
+    constructor(word) {
+        this.word = null;
+        this.value = null;
+        this.color = null;
+    }
+
+    createCard() {
+
+    }
+
+    flipCard() {
+        // flips card when it is picked
+    }
+}
---
+++ spymaster.js
@@ -0,0 +1,14 @@
+class Spymaster {
+    constructor() {
+        this.wordClue = null;
+        this.numberClue = null;
+    }
+
+    pickOpponentCard() {
+
+    }
+
+    clickHandler() {
+        // when submit button clicked, set this.wordClue and this.numberClue;
+    }
+}
