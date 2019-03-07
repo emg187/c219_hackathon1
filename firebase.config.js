e0db368177faf6b78c8ba09029cc31c359bec730
Christopher-Gustavsson <chrisgustavsson1@gmail.com>
2019-03-06 21:19:35 -0800

    Learning Firebase Pushing for team members to see code in branch. Working on uploading correct data to database.

---
+++ firebase.config.js
@@ -0,0 +1,8 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBR11vt14ucQep2QHb5N2MDjcOC-qNfoTs",
+    authDomain: "codenames-lfz.firebaseapp.com",
+    databaseURL: "https://codenames-lfz.firebaseio.com",
+    projectId: "codenames-lfz",
+    storageBucket: "codenames-lfz.appspot.com",
+    messagingSenderId: "308528029159"
+  };
--- main.js
+++ main.js
@@ -1,14 +1,44 @@
+
 $(document).ready(initializeApp);
 
 var game = null;
+var codeNamesDb = null;
 
 function initializeApp() {
+    codeNamesDb = new GenericFBModel('CodeNames Database',uploadCardToDb);
     game = new Gameboard();
     game.addCard();
 
     clickHandler();
+
+
+    // var dbRoot = codeNamesDb.db.database();
+    // var cardsRef = dbRoot.child(allCardsObj);
+
+    // var chrisCard = game.allCards.Chris;
+    // uploadCardToDb(chrisCard);
+   var text = "chris";
 }
 
 function clickHandler() {
     $(".guessBox").on('click', game.checkGuess);
 }
+
+function uploadCardToDb(card)
+{
+    console.log('saving');
+    codeNamesDb.saveState({
+        cards: {
+           0 : text,
+           1 : "bye"
+        },
+        word: "David",
+        type: "blue",
+        haha: {
+            0: 'yo',
+            1: 'wee'
+        }
+    })     
+}
+    
+
